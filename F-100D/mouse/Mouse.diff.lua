268f6c11c96bd38fe803f289272116519eabee8d
dtruck260 <53311656+dtruck260@users.noreply.github.com>
2026-07-12 16:22:16 -0400

    CommsMenu

---
+++ F-100D/mouse/Mouse.diff.lua
@@ -0,0 +1,13 @@
+local diff = {
+	["keyDiffs"] = {
+		["d179pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "MOUSE_BTN4",
+				},
+			},
+			["name"] = "Communication menu",
+		},
+	},
+}
+return diff
