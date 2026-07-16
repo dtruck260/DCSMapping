16f85d7cad213b2436eded4ac1939496cfc42f67
dtruck260 <53311656+dtruck260@users.noreply.github.com>
2026-07-16 18:30:54 -0400

    macros
    for kneeboard and command menu in default layer, engine detent and ignition in layer 3 (fn)

--- CommandMenu/keyboard/Keyboard.diff.lua
+++ CommandMenu/keyboard/Keyboard.diff.lua
@@ -1,5 +1,27 @@
 local diff = {
 	["keyDiffs"] = {
+		["d1887pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Up",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "Command Menu Next Item",
+		},
+		["d1888pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Right",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "Command Menu Select Item",
+		},
 		["d979pnilunilcdnilvdnilvpnilvunil"] = {
 			["name"] = "Command switch dialog",
 			["removed"] = {
