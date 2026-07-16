9062ebb3db2066ebf8bc97ab6d6a63f5463a893a
dtruck260 <53311656+dtruck260@users.noreply.github.com>
2026-07-15 20:05:41 -0400

    MacroStart
    Macros to use custom keys for ignition and idle

---
+++ F-100D/keyboard/Keyboard.diff.lua
@@ -0,0 +1,24 @@
+local diff = {
+	["keyDiffs"] = {
+		["d3021pnilunilcd10vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "End",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "Throttle Detent - IDLE",
+			["removed"] = {
+				[1] = {
+					["key"] = "Home",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+		},
+	},
+}
+return diff
---
+++ F-5E-3/keyboard/Keyboard.diff.lua
@@ -0,0 +1,105 @@
+local diff = {
+	["keyDiffs"] = {
+		["d3001pnilu3001cd6vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Home",
+					["reformers"] = {
+						[1] = "RAlt",
+					},
+				},
+			},
+			["name"] = "Left Engine Start Button",
+			["removed"] = {
+				[1] = {
+					["key"] = "C",
+					["reformers"] = {
+						[1] = "LCtrl",
+						[2] = "LShift",
+					},
+				},
+			},
+		},
+		["d3002pnilu3002cd6vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Home",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "Right Engine Start Button",
+			["removed"] = {
+				[1] = {
+					["key"] = "V",
+					["reformers"] = {
+						[1] = "LCtrl",
+						[2] = "LShift",
+					},
+				},
+			},
+		},
+		["d311pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "End",
+					["reformers"] = {
+						[1] = "RAlt",
+					},
+				},
+			},
+			["name"] = "Throttle (Left) - IDLE",
+			["removed"] = {
+				[1] = {
+					["key"] = "Home",
+					["reformers"] = {
+						[1] = "RAlt",
+					},
+				},
+			},
+		},
+		["d312pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "End",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "Throttle (Right) - IDLE",
+			["removed"] = {
+				[1] = {
+					["key"] = "Home",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+		},
+		["d313pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "Throttle (Left) - OFF",
+			["removed"] = {
+				[1] = {
+					["key"] = "End",
+					["reformers"] = {
+						[1] = "RAlt",
+					},
+				},
+			},
+		},
+		["d314pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "Throttle (Right) - OFF",
+			["removed"] = {
+				[1] = {
+					["key"] = "End",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+		},
+	},
+}
+return diff
