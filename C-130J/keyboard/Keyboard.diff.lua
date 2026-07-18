4c5478edd178e3a5b9f3f7288cd4036f4e853313
dtruck260 <53311656+dtruck260@users.noreply.github.com>
2026-07-18 10:22:49 -0400

    Consistancy

--- C-130J/keyboard/Keyboard.diff.lua
+++ C-130J/keyboard/Keyboard.diff.lua
@@ -1,5 +1,21 @@
 local diff = {
 	["keyDiffs"] = {
+		["d1587pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Up",
+				},
+			},
+			["name"] = "Kneeboard ON/OFF",
+		},
+		["d15pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Down",
+				},
+			},
+			["name"] = "F10 Theater map view",
+		},
 		["d193pnilu194cdnilvdnilvpnilvunil"] = {
 			["name"] = "Aircraft Pitch Down",
 			["removed"] = {
@@ -8,13 +24,45 @@ local diff = {
 				},
 			},
 		},
-		["d3002pnilunilcd43vd1vpnilvunil"] = {
-			["added"] = {
+		["d195pnilu196cdnilvdnilvpnilvunil"] = {
+			["name"] = "Aircraft Pitch Up",
+			["removed"] = {
 				[1] = {
-					["key"] = "Up",
+					["key"] = "Down",
 				},
 			},
-			["name"] = "Yoke - CMS Dispense",
+		},
+		["d197pnilu198cdnilvdnilvpnilvunil"] = {
+			["name"] = "Aircraft Bank Left",
+			["removed"] = {
+				[1] = {
+					["key"] = "Left",
+				},
+			},
+		},
+		["d199pnilu200cdnilvdnilvpnilvunil"] = {
+			["name"] = "Aircraft Bank Right",
+			["removed"] = {
+				[1] = {
+					["key"] = "Right",
+				},
+			},
+		},
+		["d3001pnilunilcd95vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Right",
+				},
+			},
+			["name"] = "Kneeboard Next Page",
+		},
+		["d3002pnilunilcd95vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Left",
+				},
+			},
+			["name"] = "Kneeboard Previous Page",
 		},
 	},
 }
